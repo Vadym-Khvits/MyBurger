20c57481c8e02a18ec521ec6a20da0e4d3150434
vadym.khvits <vadym.khvits@sigma.software>
2019-01-23 22:48:02 +0200

    #08-25 Added Toolbar and Logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png';
+import * as classes from './Logo.css';
+
+const logo = (props: any) => (
+    <div className={classes.Logo} style={{height: props.height}}>
+        <img src={burgerLogo} alt="MyBurger" />
+    </div>
+);
+
+export default logo;
---
+++ src/components/Navigation/Toolbar/Toolbar.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Logo from '../../Logo/Logo';
+import * as classes from './Toolbar.css';
+// import NavigationItems from '../NavigationItems/NavigationItems';
+// import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
+const toolbar = ( props: any ) => (
+    <header className={classes.Toolbar}>
+        {/* <DrawerToggle clicked={props.drawerToggleClicked} /> */}
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            {/* <NavigationItems /> */}
+        </nav>
+    </header>
+);
+
+export default toolbar;
