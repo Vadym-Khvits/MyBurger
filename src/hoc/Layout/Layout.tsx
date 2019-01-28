81bcefc929b734f78bd76b178872071152f8e62c
vadym.khvits <vadym.khvits@sigma.software>
2019-01-29 01:20:19 +0200

    #08-38 Moved Layout to 'hoc' folder

--- src/hoc/Layout/Layout.tsx
+++ src/hoc/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Aux from '../../hoc/_Aux';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import * as classes from './Layout.css';
 
 interface OwnStateProps {
