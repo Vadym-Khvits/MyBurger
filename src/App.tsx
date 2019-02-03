998126710bda60ce97790023a37b22900ba2dd14
vadym.khvits <vadym.khvits@sigma.software>
2019-02-04 00:53:21 +0200

    #12-03 Added Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Layout>
-          <BurgerBuilder/>
-        </Layout>
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+              <Route path="/chekout" component={Checkout} />
+              <Route path="/" exact={true} component={BurgerBuilder} />              
+            </Switch>
+          </Layout>
+        </BrowserRouter>
       </div>
     );
   }
